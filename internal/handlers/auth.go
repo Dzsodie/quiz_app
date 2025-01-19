64729380bed9f82be98b15578e26c2ea4791a220
Dzsodie <drmakarazs@gmail.com>
2025-01-19 20:34:41 +0100

    auth handler fix for server

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -88,54 +88,47 @@ func (h *AuthHandler) LoginUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := utils.ValidateSessionStore(); err != nil {
-		logger.Error("Session store not initialized", zap.Error(err))
-		http.Error(w, `{"message":"session store - Internal server error"}`, http.StatusInternalServerError)
-		return
-	}
-
 	sessionToken, err := utils.GenerateSessionToken()
 	if err != nil {
-		logger.Warn("Failed to generate session token", zap.Error(err))
-		http.Error(w, `{"message":" genereate - Internal server error"}`, http.StatusInternalServerError)
+		logger.Error("Failed to generate session token", zap.Error(err))
+		http.Error(w, `{"message":"Internal server error"}`, http.StatusInternalServerError)
 		return
 	}
 
-	utils.SessionDB[sessionToken] = user.Username
-
-	session, err := utils.SessionStore.Get(r, "quiz-session")
-	if err != nil {
-		logger.Warn("Failed to retrieve session", zap.Error(err))
-		http.Error(w, `{"message":"retrieve - Internal server error"}`, http.StatusInternalServerError)
-		return
-	}
-
-	session.Values["username"] = user.Username
-	session.Values["session_token"] = sessionToken
 	userID, err := h.AuthService.GetUserID(user.Username)
 	if err != nil {
 		logger.Error("Failed to retrieve user ID", zap.Error(err))
 		http.Error(w, `{"message":"Internal server error"}`, http.StatusInternalServerError)
 		return
 	}
+
+	session, err := utils.SessionStore.Get(r, "quiz-session")
+	if err != nil {
+		logger.Warn("Failed to retrieve session", zap.Error(err))
+		http.Error(w, `{"message":"Internal server error"}`, http.StatusInternalServerError)
+		return
+	}
+
+	session.Values["username"] = user.Username
+	session.Values["session_token"] = sessionToken
 	session.Values["userID"] = userID
 
 	if err := session.Save(r, w); err != nil {
 		logger.Error("Failed to save session", zap.Error(err))
-		http.Error(w, "Failed to create session", http.StatusInternalServerError)
+		http.Error(w, `{"message":"Internal server error"}`, http.StatusInternalServerError)
 		return
 	}
-	logger.Debug("Session saved successfully", zap.Any("session_values", session.Values))
 
 	response := map[string]string{
 		"message":       "Login successful",
 		"session_token": sessionToken,
 		"username":      user.Username,
-		"userID":        user.UserID,
+		"userID":        userID,
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(response); err != nil {
 		logger.Error("Failed to send response", zap.Error(err))
 	}
+
 }
